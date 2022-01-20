e6832d957f166ec5d4dad60592058f6d27cc2533
Fernando Jorge <79945109+nandowastaken@users.noreply.github.com>
2022-01-19 22:17:18 -0300

    Mudanças do projeto
    Até agora, toda a classe ManusearJogo, que controla o funcionamento do jogo, foram convertidas para funcionar com arrays, de modo que a quantidade de jogadores é escalável (você pode ter quantos quiser).
    
    As classes das cartas foram criadas, porém, CartaNaipe está causando um bug que gera um JavaNullExceptionError nos jogadores. 
    
    Fora isso, falta montar no Start o jogo chamando as classes e seus métodos.

---
+++ src/jogo/Jogador.java
@@ -0,0 +1,61 @@
+package jogo;
+public class Jogador {
+	private String nome;
+	private int posPodio;
+	private int posFinal;
+	private int carta;
+	private int carta_com_naipe;
+	private int pontuacao;
+	
+	Jogador(String nome_dele) {
+		this.nome = nome_dele;
+	}
+	
+	public int getCarta() {
+		return carta;
+	}
+	
+	public void setCarta(int nome_da_carta) {
+		this.carta = nome_da_carta;
+	}
+	
+	public int getCartaComNaipe() {
+		return this.carta_com_naipe;
+	}
+	
+	public void setCartaComNaipe(int numero) {
+		this.carta_com_naipe = numero; 
+	}
+	
+	public void addPonto(int valor) {
+		this.pontuacao += valor;
+	}
+	
+	public int getPontos() {
+		return this.pontuacao;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	// Acrescenta uma posição no podio
+	public void addPosPodio() {
+		this.posPodio++;
+	}
+	
+	public int getPosPodio() {
+		return this.posPodio;
+	}
+	
+	public void addPosFinal() {
+		this.posFinal++;
+	}
+	
+	public int getPosFinal() {
+		return this.posFinal;
+	}
+	
+	
+	
+}
